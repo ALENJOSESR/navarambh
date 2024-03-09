0c0c742fd8877ef56e036e410f49fc7af6235f27
Alen Joses <alenjosesr@gmail.com>
2024-03-09 08:28:20 +0530

    v3

--- js/main.js
+++ js/main.js
@@ -67,6 +67,38 @@
         return false;
     });
 
+    // Gallery
+    /*	gallery */
+    $(document).ready(function(){
+        $(".fancybox").fancybox();
+        $(".filter-button").click(function(){
+            var value = $(this).attr('data-filter');
+
+            if(value === "all")
+            {
+                $('.filter').show('1000');
+            }
+            else
+            {
+                $(".filter").not('.'+value).hide('3000');
+                $('.filter').filter('.'+value).show('3000');
+
+            }
+
+            if ($(".filter-button").removeClass("active")) {
+                $(this).removeClass("active");
+            }
+            $(this).addClass("active");
+        });
+    });
+    /*	end gallery */
+
+    $(document).ready(function(){
+        $(".fancybox").fancybox({
+            openEffect: "none",
+            closeEffect: "none"
+        });
+    });
 
     // Header carousel
     $(".header-carousel").owlCarousel({
